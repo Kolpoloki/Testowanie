30d119c9e3fa123fb06a530fbfefa1637f28f0a6
Kolpoloki <patryk2008nyy@gmail.com>
2023-10-04 09:11:14 +0200

    first

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 12;
+        int b = 45;
+        System.out.println(a + b + " wynik");
+        System.out.println("wynik " + a + b);
+        System.out.println((double) b / a);
+    }
+}
