c9a691179bc8527d50613390088a73ab6667c2d8
Kolpoloki <patryk2008nyy@gmail.com>
2023-10-04 09:38:14 +0200

    third

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -6,5 +8,14 @@ public class Main {
         System.out.println(a + b + " wynik");
         System.out.println("wynik " + a + b);
         System.out.println((double) b / a);
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Poaj pierwszą liczbę");
+        double number1 = scan.nextDouble();
+        double number2 = scan.nextDouble();
+        double result = number1 * number2;
+        System.out.println("Wynik mnożenia to: " + result);
+        System.out.printf("Wynik mnożenia liczby %f i liczby %f to: %.2f\n",number1, number2, result);
+        System.out.println("Wynik mnożenia liczby " + number1 + " i liczby " + number2 +" to: "+ result);
     }
 }
