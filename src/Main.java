04a893c48c6fc2638443f7c3ebe3bc4561dfc2f4
Kolpoloki <patryk2008nyy@gmail.com>
2023-10-04 10:33:48 +0200

    fourth

--- src/Main.java
+++ src/Main.java
@@ -17,5 +17,14 @@ public class Main {
         System.out.println("Wynik mnożenia to: " + result);
         System.out.printf("Wynik mnożenia liczby %f i liczby %f to: %.2f\n",number1, number2, result);
         System.out.println("Wynik mnożenia liczby " + number1 + " i liczby " + number2 +" to: "+ result);
+        System.out.println(2 > 3);
+//        souf ----->   %f == double   %d == integer  %s == string %b == boolean
+//        uppercase string == %S uppercase boolean == %B
+       String str = "Ala ma kota.";
+       boolean isTrue = true;
+       boolean isTrue1 = false;
+        System.out.printf("Tekst to: %s - to jest %b\n", str, isTrue);
+        System.out.printf("Tekst to: %S - to jest %B\n", str, isTrue);
+        System.out.printf("Liczba %d jest równa %f. - To zdanie jest %B. Natomiast zdanie: %s - %b. \n", a, number1, isTrue1, str, isTrue);
     }
 }
